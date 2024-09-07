ee57da8c16177841f294a8f14c2a485c9e03dc15
gibz <gibsonate123@gmail.com>
2024-09-07 17:17:12 +0300

    mern stack update

---
+++ backend/models/products.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String, 
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    images: [{
+        type: String,
+        required: true
+    }],
+}, {
+    timestamps: true
+}
+)
+
+
+const Product = mongoose.model("Product", productSchema);
+
+
+export default Product;
